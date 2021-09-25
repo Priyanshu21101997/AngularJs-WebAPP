f6a2a53178b0eccdce935118119222773a938edb
Priyanshu <priyanshu21101997@gmail.com>
2021-09-25 19:42:27 +0530

    AngularJs application

---
+++ builds/angular/js/controllers.js
@@ -0,0 +1,35 @@
+var myControllers = angular.module('myControllers',[]);
+
+myControllers.controller('SearchController',function MyController($scope,$http){
+    $http.get('js/data.json').then(function(response){
+        $scope.artists = response.data ;
+        // We can also change the value of the model which we have generated in html in here 
+        $scope.artistOrder = 'reknown';
+        // $scope.artistOrder = 'name';
+    });
+});
+
+myControllers.controller('DetailsController',function MyController($scope,$http,$routeParams){
+    $http.get('js/data.json').then(function(response){
+        $scope.artists = response.data ;
+        $scope.whichItem = $routeParams.itemId;
+
+        if($routeParams.itemId>0){
+            $scope.prevItem = Number($routeParams.itemId) - 1 ;
+        }
+        else{
+            $scope.prevItem = $scope.artists.length-1;
+        }
+
+        if($routeParams.itemId<$scope.artists.length - 1){
+            $scope.nextItem = Number($routeParams.itemId) + 1 ;
+        }
+        else{
+            $scope.nextItem = 0;
+        }
+        // We can also change the value of the model which we have generated in html in here 
+        // $scope.artistOrder = 'reknown';
+        // $scope.artistOrder = 'name';
+    });
+});
+    
